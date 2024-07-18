df6c1b00e254fdb755a05f68aa32b922e9624b06
phscavallini <megaph2008@gmail.com>
2024-07-18 19:46:17 -0300

    fixing error

--- src/ServerScriptService/KnitServer.server.lua
+++ src/ServerScriptService/KnitServer.server.lua
@@ -1,31 +1,17 @@
 local Players = game:GetService("Players")
+local ServerStorage = game:GetService("ServerStorage")
 local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
 
--- Create the service:
-local MoneyService = Knit.CreateService {
-    Name = "MoneyService",
-    _PlayerMoney =  {},
-    _DefaultValue = 0
-}
+--[[
+for i, v in pairs(ServerStorage.Source:GetDescendants()) do
+    if v:IsA("ModuleScript") and v.Name:match("Service$") then
+        require(v)
+    end
+end]]
 
--- Add some methods to the service:
-Players.PlayerAdded:Connect(function(player : Player)
-    MoneyService._PlayerMoney[player] = MoneyService:GetMoney(player) or 0
+-- mt melhor (diminui o runtime (na teoria))
+Knit.AddServices(ServerStorage.Source.Money)
+
+Knit.Start():andThen(function()
+    print("Knit started")
 end)
-
-function MoneyService:GetMoney(player): number
-    -- Do some sort of data fetch
-    return self._PlayerMoney[player]
-end
-
-function MoneyService:GiveMoney(player, amount): any
-    -- Do some sort of data fetch
-    local money = self:GetMoney(player)
-    self._PlayerMoney[player] = (money + amount)
-end
-
-function MoneyService.Client:GetMoney(player : Player)
-    return self.Server:GetMoney(player)
-end
-
-Knit.Start():catch(warn)
--- src/StarterPlayerScripts/KnitClient.client.lua
+++ src/StarterPlayerScripts/KnitClient.client.lua
@@ -1,12 +1,6 @@
 -- Client-side code
 local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
-Knit.Start():catch(warn):await()
 
-local MoneyService = Knit.GetService("MoneyService")
-
-MoneyService:GetMoney():andThen(function(money)
-    print(money)
-end)
-
--- Don't want to use promises? When you start Knit on the client,
--- set the ServicePromises option to false:
+Knit.Start():andThen(function()
+    print("Knit started")
+end):catch(warn)
