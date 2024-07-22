441561b6b72ffd8a318e8c496fe850ad92b4e154
phscavallini <megaph2008@gmail.com>
2024-07-21 23:57:53 -0300

    Controllers and middlewares

--- src/ServerScriptService/KnitServer.server.lua
+++ src/ServerScriptService/KnitServer.server.lua
@@ -3,12 +3,14 @@ local ServerStorage = game:GetService("ServerStorage")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
 
--- knit run
-local MundialService = require(ServerStorage.Source.Services.MundialService)
+local addedServices = {}
+
+for _, v in pairs(ServerStorage.Source:GetDescendants()) do
+    if v:IsA("ModuleScript") and v.Name:match("Service$") then
+        require(v)
+    end
+end
 
 Knit.Start():andThen(function()
     print("Knit started")
 end)
-
-MundialService:CriarMundial(100)
-print(MundialService:TemQuantosMundialKrai())
--- src/StarterPlayerScripts/KnitClient.client.lua
+++ src/StarterPlayerScripts/KnitClient.client.lua
@@ -1,6 +1,31 @@
--- Client-side code
+local ProximityPromptService = game:GetService("ProximityPromptService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
 local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
 
-Knit.Start():andThen(function()
+for _, v in pairs(ReplicatedStorage.Source:GetDescendants()) do
+    if v:IsA("ModuleScript") and v.Name:match("Controller$") then
+        require(v)
+    end
+end
+
+--[[
+
+-- disabled client promises
+
+Knit.Start({ServerPromises = false}):andThen(function()
+    print("Knit started")
+end):catch(warn)
+
+]]
+
+Knit.Start({
+    InboundMiddleware = {
+        function(args)
+            print("MIDDLEWARE", args)
+            return true
+        end,
+    }
+}):andThen(function()
     print("Knit started")
 end):catch(warn)
